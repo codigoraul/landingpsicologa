ee696008185e6667f226d1b6f0f31762be441556
Raul <codigoraul@gmail.com>
2026-03-24 14:32:24 -0300

    Change base path from /prueba to root / for production deployment

--- public/contacto-config.php
+++ public/contacto-config.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-  'BASE_PATH' => '/prueba',
+  'BASE_PATH' => '/',
   'SITE_URL' => 'https://ckdelap.cl',
   'TO_EMAIL' => '',
   'FROM_EMAIL' => 'formulario@ckdelap.cl',
